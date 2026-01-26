7ec8ded37395068131bb454c32479bca77aecfe3
A-lex-Ra <rastorguev2047@gmail.com>
2026-01-26 16:19:48 +0300

    improving diff -> str conversion (patch string)...

--- src/modules/processor.py
+++ src/modules/processor.py
@@ -4,14 +4,14 @@ from src.db_models import RepoMapping
 
 
 def _diff_to_str(d):
-    diff_text = d.diff.decode("utf-8", errors="ignore")
+    diff_text = d.diff.decode("utf-8", errors="ignore") if isinstance(d.diff, bytes) else d.diff
 
-    return f"""{
+    return "{" + f"""
         "change_type": {d.change_type},
         "old_path": {d.a_path},
         "new_path": {d.b_path},
-        "diff": {diff_text},
-    })"""
+        "diff": {diff_text}
+    """ + "}"
 
 
 class DiffProcessor:
@@ -30,14 +30,17 @@ class DiffProcessor:
                 # Or, diff against the empty tree (full codebase). 
                 # Let's try diffing against HEAD~1 (changes in latest commit)
                 if repo.head.commit.parents:
-                    diffs = repo.head.commit.diff("HEAD~1")
+                    diffs = repo.head.commit.diff("HEAD~1", create_patch=True)
+                    diffs = list(map(str, diffs))
                 else:
                     # Initial commit
                     diffs = [repo.git.show("HEAD")]
             else:
-                diffs = repo.commit(new_commit).diff(mapping.last_processed_commit)
-            print(list(map(_diff_to_str, diffs)))
-            return list(map(_diff_to_str, diffs))
+                # will be problematic when rebase etc
+                diffs = repo.commit(new_commit).diff(mapping.last_processed_commit, create_patch=True)
+                diffs = list(map(str, diffs))
+
+            return diffs
         except Exception as e:
             print(f"Error getting diff for {mapping.source_path}: {e}")
             return ""
