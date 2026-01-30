5b9f478e85cdb441b5544169f1e34637bcd8e380
A-lex-Ra <rastorguev2047@gmail.com>
2026-01-30 16:12:34 +0300

    Improve logs, debug out-of-context

--- src/modules/generator.py
+++ src/modules/generator.py
@@ -1,20 +1,43 @@
 import json
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.callbacks.base import CallbackManagerMixin
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain.messages import HumanMessage, AIMessage
 from langchain_community.agent_toolkits import FileManagementToolkit
 from src.core.llm import LLMFactory
+from src.core.logger import get_logger
 from src.db_models import RepoMapping
 from src.core.events import DocumentationGeneratedEvent
-from typing import Dict, List
+from typing import Dict, List, Any
 
 from pathlib import Path
+
+logger = get_logger(__name__)
+
+
+class ToolMixin(CallbackManagerMixin):
+    def on_tool_start(
+            self,
+            serialized: dict[str, Any],
+            input_str: str,
+            *,
+            tags: list[str] | None = None,
+            metadata: dict[str, Any] | None = None,
+            inputs: dict[str, Any] | None = None,
+            **kwargs: Any,
+    ) -> Any:
+        logger.info("tool called, input: %s", input_str)
+
+
+tool_callback = ToolMixin()
+
 
 @tool
 def list_repo_files(root: str) -> List[str]:
     """List all files in the repository. I recommend to check './src'"""
+    logger.info("list_repo_files called with arg root: %s", root)
     return [
         str(p) for p in Path(root).rglob("*")
         if p.is_file()
@@ -24,6 +47,7 @@ def list_repo_files(root: str) -> List[str]:
 @tool
 def read_repo_file(path: str) -> str:
     """Read a repository file and return its contents. Relative paths recommended."""
+    logger.info("read_repo_file called with arg path: %s", path)
     try:
         return Path(path).read_text()
     except Exception as e:
@@ -44,7 +68,7 @@ class DocumentationGenerator:
     def __init__(self, provider: str = "ollama", model: str = "gpt-oss:20b"):
         self.llm = LLMFactory.create_llm(provider, model)
         self.parser = JsonOutputParser()
-        
+
         # We instruct the model to return a JSON object where keys are filenames and values are markdown content.
         self.prompt = """
             You are an expert technical writer. 
@@ -75,9 +99,9 @@ class DocumentationGenerator:
             tools = docs_toolkit.get_tools()
             tools.append(list_repo_files)
             tools.append(read_repo_file)
-            agent = create_agent(model=self.llm, tools=tools, system_prompt=self.prompt.format(diff=str(diffs)))
+            agent = create_agent(model=self.llm, system_prompt=self.prompt.format(diff=str(diffs)))
             # Invoke the chain
-            result = agent.invoke({"messages": []})
+            result = agent.invoke({"messages": []}, config={"callbacks": [tool_callback]})
             print(result['messages'][0].content)
             return DocumentationGeneratedEvent(
                 repo_id=mapping.id,
